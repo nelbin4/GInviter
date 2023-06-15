a68a269056c2ec72c30106e015975a2efb124069
nelbin4 <nelbinbinag@yahoo.com>
2023-06-15 16:49:45 +0800

    Update GINVITER.lua

--- GINVITER.lua
+++ GINVITER.lua
@@ -1,18 +1,5 @@
--- GInviter.lua
--- Author: nelbin4
--- Version: 1.X
--- This addon is for World of Warcraft client patch 3.3.5a (Wrath of the Lich King)
--- Description: GInviter is an addon that facilitates automatic guild member recruitment in World of Warcraft.
--- It searches for potential recruits based on specified criteria, such as zone or class, and sends guild invitations accordingly.
--- This file contains the main functionality of the GInviter addon, including search, exclusion lists, UI elements, and event handling.
--- For more information and updates, visit github.com/nelbin4/ginviter.
---
--- ###########################################################
--- ## Variables
--- ###########################################################
---
 -- Search mode "zone" or "class"
-local SearchMode = "class"
+local SearchMode = "zone"
 
 -- Add or remove zones to search here
 local SearchZone = { "Dalaran", "The Ruby Sanctum" }
@@ -44,7 +31,7 @@ local currentZone = 1
 local currentClass = 1
 local excludeList = {}
 
--- Create the main frame
+
 local frame = CreateFrame("Frame", "GINVITERFrame", UIParent)
 frame:SetSize(145, 135)
 frame:SetPoint("CENTER")
@@ -60,7 +47,6 @@ frame:SetBackdrop({
     insets = { left = 4, right = 4, top = 4, bottom = 4 },
 })
 
--- Create the title bar
 local titleBar = CreateFrame("Frame", nil, frame)
 titleBar:SetSize(frame:GetWidth(), 25)
 titleBar:SetPoint("TOP", 0, 0)
@@ -72,13 +58,11 @@ titleBar:SetBackdrop({
 })
 titleBar:SetBackdropColor(0.5, 0, 0.1, 1)
 
--- Create the title text
 local titleText = titleBar:CreateFontString(nil, "ARTWORK", "GameFontNormal")
 titleText:SetPoint("CENTER", titleBar, "CENTER")
 titleText:SetText("GInviter")
 titleText:SetTextColor(1, 1, 1)
 
--- Create the minimize button
 local minimizeButton = CreateFrame("Button", nil, frame)
 minimizeButton:SetSize(25, 25)
 minimizeButton:SetPoint("TOPRIGHT", frame, "TOPRIGHT", 0, 0)
@@ -86,31 +70,26 @@ minimizeButton:SetNormalTexture("Interface\\Buttons\\UI-Panel-MinimizeButton-Up"
 minimizeButton:SetHighlightTexture("Interface\\Buttons\\UI-Panel-MinimizeButton-Highlight")
 minimizeButton:SetScript("OnClick", function() frame:Hide() end)
 
--- Create the status text
 local statusText = frame:CreateFontString(nil, "ARTWORK", "GameFontNormal")
 statusText:SetPoint("TOP", titleBar, "BOTTOM", 0, -10)
 statusText:SetJustifyH("CENTER")
 statusText:SetText("Stopped")
 
--- Create the time text
 local timeText = frame:CreateFontString(nil, "ARTWORK", "GameFontNormal")
 timeText:SetPoint("TOP", statusText, "BOTTOM", 0, -10)
 timeText:SetJustifyH("CENTER")
 timeText:SetText("Time left:")
 
--- Create the time value
 local timeValue = frame:CreateFontString(nil, "ARTWORK", "GameFontNormal")
 timeValue:SetPoint("TOP", timeText, "BOTTOM", 0, -5)
 timeValue:SetJustifyH("CENTER")
 timeValue:SetText("-")
 
--- Create the error text
 local errorText = frame:CreateFontString(nil, "ARTWORK", "GameFontNormal")
 errorText:SetPoint("BOTTOMRIGHT", frame, "BOTTOMRIGHT", -10, 10)
 errorText:SetJustifyH("RIGHT")
 errorText:SetText("")
 
--- Create the start button
 local startButton = CreateFrame("Button", "GINVITERStartButton", frame, "UIPanelButtonTemplate")
 startButton:SetSize(60, 20)
 startButton:SetPoint("BOTTOMLEFT", 10, 10)
@@ -122,7 +101,6 @@ else
 end
 startButton:SetScript("OnClick", function() GINVITER_StartSearch() end)
 
--- Create the stop button
 local stopButton = CreateFrame("Button", "GINVITERStopButton", frame, "UIPanelButtonTemplate")
 stopButton:SetSize(60, 20)
 stopButton:SetPoint("BOTTOMLEFT", startButton, "BOTTOMRIGHT", 5, 0)
@@ -134,46 +112,44 @@ else
 end
 stopButton:SetScript("OnClick", function() GINVITER_StopSearch(); GINVITER_StopSearch() end)
 
--- Function to add a player to the exclude list
+
 local function GINVITER_AddToExcludeList(playerName)
     excludeList[playerName] = (excludeList[playerName] or 0) + 1
 end
 
--- Function to check if a player is excluded from invites
 local function GINVITER_IsExcluded(playerName)
     return excludeList[playerName] and excludeList[playerName] >= maxInvites
 end
 
--- Function to handle the GINVITER command
 local function GINVITER_Command(args)
     if (args == "show") then
-        frame:Show() -- Show the frame
+        frame:Show()
     elseif (args == "hide") then
-        frame:Hide() -- Hide the frame
+        frame:Hide()
     else
-        ChatFrame1:AddMessage("Usage: /GINVITER [show/hide]") -- Display usage instructions in the chat
+        ChatFrame1:AddMessage("Usage: /GINVITER [show/hide]")
     end
 end
 
--- Function to start the search
 function GINVITER_StartSearch()
     if CanGuildInvite() then
-        local initialSearchZone = table.clone(SearchZone) -- Create a clone of the SearchZone table
-        local initialSearchClass = table.clone(SearchClass) -- Create a clone of the SearchClass table
-        statusText:SetText("Searching") -- Set the status text to indicate searching
-        searching = true -- Set searching flag to true
-        lastSearchTime = time() -- Set the last search time to the current time
-        GINVITER_SendSearch() -- Initiate the search
-        timeLeft = 0 -- Reset the time left
-        currentZone = 1 -- Reset the current zone index
-        currentClass = 1 -- Reset the current class index
+        local initialSearchZone = table.clone(SearchZone)
+        local initialSearchClass = table.clone(SearchClass)
+        statusText:SetText("Searching")
+        searching = true
+        lastSearchTime = time()
+        GINVITER_SendSearch()
+        timeLeft = 0
+        currentZone = 1
+        currentClass = 1
         startButton:Disable()  -- Disable the Start button
         stopButton:Enable()  -- Enable the Stop button
     else
-        GINVITER_StopSearch() -- Stop the search if not in a guild
-        print("We are not in a guild. Join a guild to use GInviter.") -- Display a message indicating not in a guild
+        GINVITER_StopSearch()
+        print("We are not in a guild. Join a guild to use GInviter.")
     end
 end
+
 
 -- Function to stop the search
 function GINVITER_StopSearch()
@@ -183,44 +159,41 @@ function GINVITER_StopSearch()
     stopButton:Disable()  -- Disable the Stop button
 end
 
+
 -- Function to restart the search
 function GINVITER_RestartSearch()
     if CanGuildInvite() then
-        excludeList = {} -- Clear the exclude list
-		searching = true -- Set searching flag to true
-        lastSearchTime = time() -- Reset the last search time
-        GINVITER_SendSearch() -- Initiate a new search
-        timeLeft = 0 -- Reset the time left
-        currentZone = 1 -- Reset the current zone index
-        currentClass = 1 -- Reset the current class index
-		print("GInviter: Restarting..") -- Display a message indicating restart
+        excludeList = {}
+		searching = true
+        lastSearchTime = time()
+        GINVITER_SendSearch()
+        timeLeft = 0
+        currentZone = 1
+        currentClass = 1
+		startButton:Disable()  -- Disable the Start button
+        stopButton:Enable()  -- Enable the Stop button
+		print("GInviter: Restarting..")
     else
-        GINVITER_StopSearch() -- Stop the search if not in a guild
-        print("We are not in a guild. Join a guild to use GInviter.") -- Display a message indicating not in a guild
+        GINVITER_StopSearch()
+        print("We are not in a guild. Join a guild to use GInviter.")
     end
 end
 
--- Table cloning function for restart purposes
+-- Table Cloning for Restart purposes
 function table.clone(...)
-    -- Collect the tables to be cloned
     local clones = {...}
-    -- Create a new table to store the cloned result
     local result = {}
     
-    -- Iterate over each table to be cloned
     for i, clone in ipairs(clones) do
-        -- Iterate over the key-value pairs of each table
         for key, value in pairs(clone) do
-            -- If the value is a table, recursively clone it
             if type(value) == "table" then
                 result[key] = table.clone(value) -- Recursively clone nested tables
             else
-                result[key] = value -- Copy non-table values directly
+                result[key] = value
             end
         end
     end
     
-    -- Return the cloned result table
     return result
 end
 
@@ -231,23 +204,28 @@ function GINVITER_SendSearch()
 
     local whoString = ""
     if (SearchMode == "zone") then
-        -- Randomly select a zone from the remaining list
-        local zoneIndex = math.random(1, #SearchZone)
+        if #SearchZone == 0 then
+            GINVITER_StopSearch()
+            return
+        end
+
+        local zoneIndex = math.random(1, #SearchZone) -- Randomly select a zone index
         local zone = SearchZone[zoneIndex]
         table.remove(SearchZone, zoneIndex) -- Remove the selected zone from the list
-        -- Construct the search query string for zone search mode
         whoString = "g-\"\" " .. level .. " z-\"" .. zone .. "\""
         statusText:SetText("Searching in\n" .. zone)
     elseif (SearchMode == "class") then
-        -- Randomly select a class from the remaining list
-        local classIndex = math.random(1, #SearchClass)
+        if #SearchClass == 0 then
+            GINVITER_StopSearch()
+            return
+        end
+
+        local classIndex = math.random(1, #SearchClass) -- Randomly select a class index
         local class = SearchClass[classIndex]
         table.remove(SearchClass, classIndex) -- Remove the selected class from the list
-        -- Construct the search query string for class search mode
         whoString = "g-\"\" " .. level .. " c-\"" .. class .. "\""
         statusText:SetText("Searching for\n" .. class)
     end
-	-- Send who string
     lastSearchTime = time()
     SendWho(whoString)
 end
@@ -255,26 +233,17 @@ end
 -- Function to handle the update
 local function GINVITER_OnUpdate(args)
     if searching then
-        -- Check if the guild is full. Stop when reached 1001 members
-        local numGuildMembers, _, _ = GetNumGuildMembers()
-        if numGuildMembers >= 1001 then
-            GINVITER_StopSearch()
-            statusText:SetText("Guild Full")
-            return
-        end
-
         local timeLeft = lastSearchTime + loopInterval - time()
         if timeLeft < 0 then
-            -- Check if the player has guild invite privileges
             if CanGuildInvite() then
-                GINVITER_SendSearch() -- Send the search query
+                GINVITER_SendSearch()
             else
                 GINVITER_StopSearch()
                 print("We don't have /ginvite privileges. Ask the Guild Master or an Officer.")
             end
         else
-            timeValue:SetText(string.format("%.0f", timeLeft)) -- Update remaining time display
-			errorText:SetText("") -- Clear error message
+            timeValue:SetText(string.format("%.0f", timeLeft))
+            errorText:SetText("")
         end
     end
 
@@ -286,64 +255,48 @@ local function GINVITER_OnUpdate(args)
         if #SearchClass == 0 then
             SearchClass = table.clone(initialSearchClass) -- Reset SearchClass to initial values
         end
-
+        
         GINVITER_RestartSearch() -- Restart the search
     end
 end
 
--- Function to handle the event
 function GINVITER_OnEvent(args)
-    -- If not currently searching, return and do nothing
-    if searching == false then
-        return
-    end
-    GINVITER_InviteWhoResults() -- Process the WHO_LIST_UPDATE event and invite eligible players
+    if (searching == false) then return end
+    GINVITER_InviteWhoResults()
 end
 
 function GINVITER_OnLoad()
 end
 
 function GINVITER_InviteWhoResults()
-    -- Get the number of results from the /who command
     local numWhos = GetNumWhoResults()
 
-    -- Loop through each result
     for index = 1, numWhos, 1 do
-        -- Get information about the character
         local charname, guildname, level, race, class, zone, classFileName = GetWhoInfo(index)
         
-        -- Check if the character is not in a guild and is not excluded and the zone is not excluded
         if (guildname == "" and not GINVITER_IsExcluded(charname) and not IsZoneExcluded(zone)) then
-            -- Add the character to the exclude list to avoid inviting them again
             GINVITER_AddToExcludeList(charname)
-            -- Send a guild invite to the character
             GuildInvite(charname)
         end
     end
 end
 
 function IsZoneExcluded(zone)
-    -- Loop through each excluded zone in the excludedZones table
     for _, excludedZone in ipairs(excludedZones) do
-        -- Check if the current zone matches an excluded zone
         if zone == excludedZone then
-            -- If a match is found, return true
             return true
         end
     end
-    -- If no match is found, return false
     return false
 end
 
-
-frame:SetScript("OnUpdate", GINVITER_OnUpdate) -- Set the update script to execute GINVITER_OnUpdate function
-frame:SetScript("OnEvent", GINVITER_OnEvent) -- Set the event script to execute GINVITER_OnEvent function
-frame:SetScript("OnLoad", GINVITER_OnLoad) -- Set the load script to execute GINVITER_OnLoad function
-frame:RegisterEvent("WHO_LIST_UPDATE") -- Register the "WHO_LIST_UPDATE" event to be tracked by the frame
-frame:SetFrameStrata("LOW") -- Set the frame's strata to "LOW" to make it appear behind other frames
-frame:SetClampedToScreen(true) -- Clamp the frame's position within the screen boundaries
-SLASH_GINVITER1 = "/GINVITER" -- Define the slash command "/GINVITER"
-SlashCmdList["GINVITER"] = GINVITER_Command -- Associate the slash command with the GINVITER_Command function
+frame:SetScript("OnUpdate", GINVITER_OnUpdate)
+frame:SetScript("OnEvent", GINVITER_OnEvent)
+frame:SetScript("OnLoad", GINVITER_OnLoad)
+frame:RegisterEvent("WHO_LIST_UPDATE")
+frame:SetFrameStrata("LOW")
+frame:SetClampedToScreen(true)
+SLASH_GINVITER1 = "/GINVITER"
+SlashCmdList["GINVITER"] = GINVITER_Command
 
 print("|cff00ff00GInviter loaded.|r Use /GINVITER. Modify file ginviter.lua inside addon folder. Check github.com/nelbin4/ginviter for updates.")
--- Print a message to the chat frame indicating that GInviter has been loaded and provide instructions for usage and updates.
